5af2a85bd70c8b7a593812fd81f1436a049b3726
BillyBernard <billybernard54@gmail.com>
2021-10-15 11:26:42 -0600

    working on tests

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
+const testArticle = {
+    id: 1,
+    headline: "made up headline",
+    createdOn: 'made up date',
+    author: "made up author",
+    summary: "made up summary",
+    body: "made up body",
+
+}
+
 test('renders component without errors', ()=> {
+    render (<Article article={testArticle}/>);
 });
 
 // test('renders headline, author from the article when passed in through props', ()=> {
